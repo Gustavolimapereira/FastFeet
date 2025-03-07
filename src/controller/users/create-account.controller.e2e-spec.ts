460fe11ba30041d299abeb99f5c760fdbb32e4f2
gustavolimapereira <gu.lim@hotmail.com>
2025-03-07 00:14:24 -0300

    update

--- src/controller/users/create-account.controller.e2e-spec.ts
+++ src/controller/users/create-account.controller.e2e-spec.ts
@@ -1,3 +1,41 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toBe(2)
+import { AppModule } from '@/app.module'
+import { PrismaService } from '@/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('Criar conta (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      cpf: '111.111.111-11',
+      password: '123456',
+      role: 'ENTREGADOR',
+    })
+
+    expect(response.statusCode).toBe(201)
+
+    const userOnDatabase = await prisma.user.findUnique({
+      where: {
+        cpf: '111.111.111-11',
+      },
+    })
+
+    expect(userOnDatabase).toBeTruthy()
+  })
 })
